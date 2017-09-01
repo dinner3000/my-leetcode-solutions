aff71e12ab7322e468d38ebb63796615a7291b87
Zhu Jiawei <ab052151zhujiawei@ab-insurance.com>
2017-09-01 16:15:52 +0800

    Init

---
+++ src/main/java/com/dinner3000/leetcode/solutions/TwoSum.java
@@ -0,0 +1,13 @@
+package com.dinner3000.leetcode.solutions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TwoSum {
+
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
+    public int[] twoSum(int[] nums, int target) {
+        return new int[2];
+    }
+}
